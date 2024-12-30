00fdb94804da6e48b6136bcc991ec13eda0d9cb9
Horacio Belardita <horaciobelardita@gmail.com>
2024-12-30 00:33:53 -0300

    add root route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,5 +2,14 @@ import { Express } from "express";
 import { createUsersRoutes } from "./users";
 
 export const createRoutes = (app: Express) => {
+  createOtherRoutes(app);
   createUsersRoutes(app);
 };
+function createOtherRoutes(app: Express) {
+  app.get("/", (_req, resp) => {
+    resp.json({
+      success: true,
+      message: "Welcome to the emoji server",
+    });
+  });
+}
